040808464662517893300cab1b7be1bca9204c91
Dmitry Korenko <depiict@gmail.com>
2020-08-22 14:53:45 +0300

    feat: add bootstrap

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,9 @@
 /* global $, AOS */
+/* eslint-disable spaced-comment */
+
+//= ../../node_modules/bootstrap/js/dist/util.js
+//= ../../node_modules/bootstrap/js/dist/scrollspy.js
+//= ../../node_modules/bootstrap/js/dist/collapse.js
 
 $(document).ready(function() {
   AOS.init();
