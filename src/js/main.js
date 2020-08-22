7b14e8f556a7d591b75d47c8027c21559dfd0bf7
Dmitry Korenko <depiict@gmail.com>
2020-08-22 19:18:18 +0300

    feat: add reviews section

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,19 @@
 
 $(document).ready(function() {
   AOS.init();
+
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    nav: false,
+    navText: '',
+    dots: true,
+    loop: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    fluidSpeed: 600,
+    autoplaySpeed: 600,
+    navSpeed: 600,
+    dotsSpeed: 600,
+    dragEndSpeed: 600,
+  });
 });
