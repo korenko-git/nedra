af3a68537bad0546984645ff5d8c147120edea6a
Dmytro Korenko <depiict@gmail.com>
2025-06-01 18:57:22 +0300

    style: format SCSS and JS files consistently
    - Add .prettierignore file to exclude HTML and Nunjucks files
    - Fix indentation and spacing in all SCSS files
    - Standardize formatting in JavaScript file
    - Ensure consistent line endings and remove trailing whitespace

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,45 @@
 /* global $, AOS */
 /* eslint-disable spaced-comment */
 
-$(document).ready(function() {
+$(document).ready(function () {
   AOS.init();
 
   // Smooth scroll for anchor links with offset
-  $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      let target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        event.preventDefault();
-        let navbarHeight = $('#navbar-top').outerHeight(); 
-        $('html, body').animate({
-          scrollTop: target.offset().top - navbarHeight 
-        }, 1000, function() {
-          let hash = $(event.target).attr('href');
-          if (window.history.pushState) {
-            window.history.pushState(null, null, hash);
-          } else {
-            window.location.hash = hash;
-          }
-        });
+  $('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+      if (
+        location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        let target = $(this.hash);
+        target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          event.preventDefault();
+          let navbarHeight = $("#navbar-top").outerHeight();
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top - navbarHeight,
+            },
+            1000,
+            function () {
+              let hash = $(event.target).attr("href");
+              if (window.history.pushState) {
+                window.history.pushState(null, null, hash);
+              } else {
+                window.location.hash = hash;
+              }
+            },
+          );
+        }
       }
-    }
-  });
+    });
 
-  $('.owl-carousel').owlCarousel({
+  $(".owl-carousel").owlCarousel({
     items: 1,
     nav: false,
-    navText: '',
+    navText: "",
     dots: true,
     loop: true,
     autoplay: true,
@@ -41,15 +51,15 @@ $(document).ready(function() {
     dragEndSpeed: 600,
   });
 
-  $('.forms').submit(function() {
+  $(".forms").submit(function () {
     $.ajax({
-      type: 'POST',
-      url: 'mail.php',
+      type: "POST",
+      url: "mail.php",
       // eslint-disable-next-line no-invalid-this
       data: $(this).serialize(),
-    }).done(function() {
-      alert('Спасибо за заявку!');
-      $('.forms').trigger('reset');
+    }).done(function () {
+      alert("Спасибо за заявку!");
+      $(".forms").trigger("reset");
     });
 
     return false;
